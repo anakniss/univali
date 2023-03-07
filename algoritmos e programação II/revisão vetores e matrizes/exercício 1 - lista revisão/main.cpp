7391ba6a65c212544905b039ee1abd3d74fdcb6e
Ana Kniss <you@example.com>
2023-03-07 16:47:01 -0300

    added exercise 1 from vetors list

--- algoritmos e programação II/revisão vetores e matrizes/exercício 1 - lista revisão/main.cpp
+++ algoritmos e programação II/revisão vetores e matrizes/exercício 1 - lista revisão/main.cpp
@@ -2,8 +2,40 @@
 
 using namespace std;
 
+int numeros[10];
+
+int valor = 1;
+int divisores = 0;
+int numero, i;
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    for(i = 0; i < 10; i++){
+        cout << "Digite o número: " << endl;
+        cin >> numero;
+
+        numeros[i] = numero;
+
+        divisores = 0;
+        valor = 1;
+
+        if(numero > 0) {
+            while(valor <= numero){
+                if(numero % valor == 0){
+                    divisores++;
+                }
+                valor++;
+            }
+            if(divisores == 2){
+                cout << "O número " << numero << " é primo!" << endl;
+            }
+            else {
+                cout << "O número " << numero << " NÃO é primo!" << endl;
+            }
+        }
+
+        else {
+            cout << "Valor negativo ou igual a zero!";
+        }
+    }
 }
