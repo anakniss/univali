b28d025f8fcea001096f6e79c65ffd69cf1719af
Ana Kniss <you@example.com>
2023-04-02 18:56:10 -0300

    added exercise 5 from iterators list

--- algoritmos e programação I/cpp/iteradores/do-while/exercício 5 - lista 3/main.cpp
+++ algoritmos e programação I/cpp/iteradores/do-while/exercício 5 - lista 3/main.cpp
@@ -4,6 +4,39 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int cont = 0;
+    float valorProduto = 0;
+    float totalVendas = 0;
+    float salarioVendedor = 0;
+    char opcao;
+    string nome;
+
+    do {
+        cout << "Informe o nome do funcionário: " << endl;
+        cin >> nome;
+        do {
+            cout << "Informe o valor do produto vendido: " << endl;
+            cin >> valorProduto;
+
+            cout << "Informe a quantidade vendida:" << endl;
+            cin >> cont;
+
+
+            salarioVendedor += (valorProduto * 0.3) * cont;
+
+
+            totalVendas += valorProduto * cont;
+
+            cout << "Este funcionário tem mais uma venda? S/N" << endl;
+            cin >> opcao;
+
+        } while(opcao == 'S');
+
+        cout << "Nome do vendedor: " << nome << endl;
+        cout << "Total de vendas: R$" << totalVendas << endl;
+        cout << "Salário do vendedor: R$" << salarioVendedor << endl;
+
+        cout << "Deseja digitar os dados de mais um vendedor? S/N" << endl;
+        cin >> opcao;
+    } while(opcao == 'S');
 }
