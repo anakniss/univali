b265056c10f07ee0cb47de0abcf9f86497fc51a8
Ana Clara Kniss <ana.kniss@meta.com.br>
2023-04-08 12:58:00 -0300

    wip

--- algoritmos e programação I/cpp/iteradores/while/exercício 6 - lista 3/main.cpp
+++ algoritmos e programação I/cpp/iteradores/while/exercício 6 - lista 3/main.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main()
 {
-    int horas = 0;
+    int horas = 1;
     int qntdBiscoitos = 1;
+    int qntdBiscoitosTotal = 1;
 
-    while(horas <= 16){
+    while(horas < 16){
         qntdBiscoitos = qntdBiscoitos * 3;
+        qntdBiscoitosTotal = qntdBiscoitos + qntdBiscoitosTotal;
         horas++;
     }
-    cout << "Valor total após 16h: " << qntdBiscoitos << endl;
+    cout << "Valor total após 16h: " << qntdBiscoitosTotal << endl;
 }
