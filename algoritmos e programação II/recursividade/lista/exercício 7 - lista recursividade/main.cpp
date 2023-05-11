3ee56fccc9b801f345be5d99a2e90a3366da23e4
Ana Clara Kniss <ana.kniss@meta.com.br>
2023-05-11 12:46:48 -0300

    added exercise 7 from recursive list

--- algoritmos e programação II/recursividade/lista/exercício 7 - lista recursividade/main.cpp
+++ algoritmos e programação II/recursividade/lista/exercício 7 - lista recursividade/main.cpp
@@ -7,14 +7,14 @@ int calculaProdEscalar(int n, int x[], int y[]){
     if(n == 1){
         return x[0] * y[0];
     }
-    return x[n-1] * y[n-1] + calculaProdEscalar(n-1, x[n], y[n]);
+    return x[n-1] * y[n-1] + calculaProdEscalar(n-1, x, y);
 }
 
 int main()
 {
-    x[TMAX] = {1, 5, 6, 7, 8};
-    y[TMAX] = {3, 4, 7, 8, 9};
-    n = 5;
+    int x[TMAX] = {1, 5, 6, 7, 8};
+    int y[TMAX] = {3, 4, 7, 8, 9};
+    int n = 5;
 
     cout << calculaProdEscalar(n, x, y);
 }
