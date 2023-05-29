74edfe8e40f09d65910a35c799770488463657dd
Ana Kniss <ackniss@gmail.com>
2023-05-29 18:03:03 -0300

    wip

--- algoritmos e programação II/alocação dinâmica de memória e lista encadeada/lista/exercício 3/main.cpp
+++ algoritmos e programação II/alocação dinâmica de memória e lista encadeada/lista/exercício 3/main.cpp
@@ -2,26 +2,76 @@
 
 using namespace std;
 
+//Escreva um procedimento que insira um novo elemento em uma lista de valores inteiros, após a
+//primeira ocorrência do valor 80, caso exista este valor.
+
 typedef struct Nodo {
-    string elemento;
+    int elemento;
     struct Nodo *proximo;
 } Lista;
 
-void insereAposNumero(Lista* lauxl, int n, string elem){
-    while(laux != NULL){
-        if(laux->valor == n){
-            Lista * nodoe = new Lista;
-            nodoe->valor = elem;
-            nodoe-> laux->prox;
-            laux->prox = nodoe;
-            return;
-        }
-        laux = laux->prox;
-    }
-}
+Lista* montaLista();
+void insereAposNumero(Lista*, int, int);
+void exibaLista(Lista *);
 
 int main()
 {
-    Lista *head = new Lista;
+    Lista *head = montaLista();
+    insereAposNumero(head, 80, 90);
+    exibaLista(head);
+}
 
+Lista* montaLista(){
+    Lista* atual, *paux, *lista = NULL;
+    char opcao;
+
+    do {
+        atual = new Lista;
+
+        cout << "Informe o numero: " << endl;
+        cin >> atual->elemento;
+
+        cin.ignore();
+
+        if(lista == NULL){
+            lista = atual;
+            paux = atual;
+        }
+        else {
+            paux->proximo = atual;
+            paux = atual;
+        }
+        cout << "Deseja adicionar outro elemento na lista? S/N" << endl;
+        opcao = toupper(cin.get());
+
+        cin.ignore();
+
+    } while(opcao != 'N');
+
+    atual->proximo = NULL;
+
+    return lista;
+}
+
+void insereAposNumero(Lista* head, int n, int elem){
+    while(head != NULL){
+        if(head->elemento == n){
+            Lista *novo = new Lista;
+            novo->elemento = elem;
+            novo->proximo = head->proximo;
+            head->proximo = novo;
+            return;
+        }
+      head = head->proximo;
+    }
+}
+
+void exibaLista(Lista *lista){
+    Lista *atual = lista;
+    while(atual != NULL){
+        cout << "Numero: " << atual->elemento << endl;
+        cout << "====================" << endl;
+
+        atual = atual->proximo;
+    }
 }
