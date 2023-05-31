51aebd0c5ec8fbf9147eb12c25c3380b35bf6238
DESKTOP-0CDRRID\Administrador <suporte@univali.br>
2023-05-30 21:51:17 -0300

    wip

---
+++ algoritmos e programação II/vector e list/lista - vector/exercício 4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+//Escreva uma função vector<int> append(vector<int> a, vector<int> b)
+//que acrescenta um vetor após outro.
+//Ex: vetor 1,4,9,16 e b 9,7,4,9,11 então append retorna o vetor 1,4,9,16,9,7,4,9,11
+
+vector<int> append(vector<int>, vector<int>);
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+vector<int> append(vector<int> a, vector<int> b){
+
+}
