91f02697f7b2b3d531219ac1c1e11678bcb999b0
Ana Kniss <ackniss@gmail.com>
2023-06-04 21:31:52 -0300

    added exercise 4

--- algoritmos e programação II/vector e list/lista - vector/exercício 4/main.cpp
+++ algoritmos e programação II/vector e list/lista - vector/exercício 4/main.cpp
@@ -1,19 +1,58 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
 
 //Escreva uma função vector<int> append(vector<int> a, vector<int> b)
 //que acrescenta um vetor após outro.
 //Ex: vetor 1,4,9,16 e b 9,7,4,9,11 então append retorna o vetor 1,4,9,16,9,7,4,9,11
 
 vector<int> append(vector<int>, vector<int>);
+void adicionaVetores(string, vector<int> &);
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    vector<int> a;
+    vector<int> b;
+    vector<int> c;
+
+    adicionaVetores("Informe um valor a ser adicionado no vetor A: ", a);
+    adicionaVetores("Informe um valor a ser adicionado no vetor B: ", b);
+
+    c = append(a, b);
+
+    for(int i=0; i<c.size() - 1; i++){
+        cout << c[i] << ", ";
+    }
+
+    cout << c.back() << ".";
+}
+
+void adicionaVetores(string mensagem, vector<int> &meuVetor){
+    char opcao;
+    int n;
+    do {
+        cout << mensagem << endl;
+        cin >> n;
+        meuVetor.push_back(n);
+        cin.ignore();
+
+        cout << "Outro? S/N" << endl;
+        opcao = toupper(cin.get());
+    } while(opcao != 'N');
 }
 
 vector<int> append(vector<int> a, vector<int> b){
+    vector<int> c;
 
+    for(int i=0; i<a.size(); i++){
+        c.push_back(a[i]);
+    }
+
+    for(int i=0; i< b.size(); i++){
+        c.push_back(b[i]);
+    }
+
+    return c;
 }
