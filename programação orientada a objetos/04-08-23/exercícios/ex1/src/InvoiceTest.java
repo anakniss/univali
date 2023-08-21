31225baf24b7811f3d0aacd635e0e729c1f861fd
Ana Kniss <ackniss@gmail.com>
2023-08-20 21:36:27 -0300

    wip

--- programação orientada a objetos/04-08-23/exercícios/ex1/src/InvoiceTest.java
+++ programação orientada a objetos/04-08-23/exercícios/ex1/src/InvoiceTest.java
@@ -7,13 +7,16 @@ public class InvoiceTest {
 
         Invoice invoice1 = new Invoice("123532", "first", 2, 1000);
 
-        System.out.printf("number: %s%n", invoice1.getNumber());
-        System.out.printf("description: %s%n", invoice1.getDescription());
-        System.out.printf("quantity: %s%n", invoice1.getQuantity());
-        System.out.printf("price: %s%n", invoice1.getPrice());
-        System.out.printf("total: %s%n", invoice1.getInvoiceAmount(invoice1.getQuantity(), invoice1.getPrice()));
-
-        System.out.println("Altered information");
+        System.out.printf("number: %s%n",
+                invoice1.getNumber());
+        System.out.printf("description: %s%n",
+                invoice1.getDescription());
+        System.out.printf("quantity: %s%n",
+                invoice1.getQuantity());
+        System.out.printf("price: %s%n",
+                invoice1.getPrice());
+        System.out.printf("total: %s%n",
+                invoice1.getInvoiceAmount(invoice1.getQuantity(), invoice1.getPrice()));
 
         System.out.println("Enter a new number value: ");
         String newValue = input.nextLine();
@@ -31,10 +34,17 @@ public class InvoiceTest {
         double newPrice = input.nextDouble();
         invoice1.setPrice(newPrice);
 
-        System.out.printf("number: %s%n", invoice1.getNumber());
-        System.out.printf("description: %s%n", invoice1.getDescription());
-        System.out.printf("quantity: %s%n", invoice1.getQuantity());
-        System.out.printf("price: %s%n", invoice1.getPrice());
-        System.out.printf("total: %s%n", invoice1.getInvoiceAmount(invoice1.getQuantity(), invoice1.getPrice()));
+        System.out.println("Altered information...");
+
+        System.out.printf("number: %s%n",
+                invoice1.getNumber());
+        System.out.printf("description: %s%n",
+                invoice1.getDescription());
+        System.out.printf("quantity: %s%n",
+                invoice1.getQuantity());
+        System.out.printf("price: %s%n",
+                invoice1.getPrice());
+        System.out.printf("total: %s%n",
+                invoice1.getInvoiceAmount(invoice1.getQuantity(), invoice1.getPrice()));
     }
 }
